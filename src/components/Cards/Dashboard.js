7575ae68869cf7bf51f2dc9cbcaa3c153b94bd48
difaananda40 <difacool40@gmail.com>
2020-06-22 03:35:46 +0800

    Make sidebar work

--- src/components/Cards/Dashboard.js
+++ src/components/Cards/Dashboard.js
@@ -2,11 +2,47 @@ import React, { Component} from 'react';
 import Sidebar from '../sidebar';
 
 export default class Dashboard extends Component{
-    
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {
+            profile: {
+                firstname: '',
+                lastname: '',
+                location: 'default',
+                description: '',
+                image: null,
+            },
+        };
+    }
+
+    componentDidMount() {
+        this.fetchProfile();
+    }
+
+    fetchProfile = () => {
+        const profile = JSON.parse(localStorage.getItem('profile'))
+        if(profile) {
+            this.setState({
+                profile
+            })
+        }
+    }
+
+    setProfile = async (data) => {
+        localStorage.setItem('profile', JSON.stringify(data));
+        this.setState({
+            profile: data
+        })
+    }
+
+    render() {
+        const { profile } = this.state;
         return(
             <div>
-                <Sidebar/>
+                <Sidebar
+                    profile={profile}
+                    setProfile={(data) => this.setProfile(data)}
+                />
                 Dashboard tab
             </div>
         )
